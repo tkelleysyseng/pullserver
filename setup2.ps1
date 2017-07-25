2b0bfabd47f74753f369241c11290dc953cf7896
Tanner Kelley <tkelley@syseng.com>
2017-07-25 12:24:13 -0400

    initial commit

---
+++ setup2.ps1
@@ -0,0 +1,46 @@
+﻿configuration Sample_xDscPullServer
+{ 
+    param  
+    ( 
+            [string[]]$NodeName = 'localhost', 
+
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string] $RegistrationKey 
+     ) 
+
+
+     Import-DSCResource -ModuleName xPSDesiredStateConfiguration
+     Import-DSCResource –ModuleName PSDesiredStateConfiguration
+
+     Node $NodeName 
+     { 
+         WindowsFeature DSCServiceFeature 
+         { 
+             Ensure = 'Present'
+             Name   = 'DSC-Service'             
+         } 
+
+         xDscWebService PSDSCPullServer 
+         { 
+             Ensure                   = 'Present' 
+             EndpointName             = 'PSDSCPullServer' 
+             Port                     = 8080 
+             PhysicalPath             = "$env:SystemDrive\inetpub\PSDSCPullServer" 
+             CertificateThumbPrint    = "AllowUnencryptedTraffic"         
+             ModulePath               = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules" 
+             ConfigurationPath        = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration" 
+             State                    = 'Started'
+             DependsOn                = '[WindowsFeature]DSCServiceFeature'     
+             UseSecurityBestPractices = $false
+         } 
+
+        File RegistrationKeyFile
+        {
+            Ensure          = 'Present'
+            Type            = 'File'
+            DestinationPath = "$env:ProgramFiles\WindowsPowerShell\DscService\RegistrationKeys.txt"
+            Contents        = $RegistrationKey
+        }
+    }
+}
